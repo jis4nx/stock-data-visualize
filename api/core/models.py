54d7b78d1a334744158f5f130e70a621820bccad
Rakibul Islam <jis4nx@gmail.com>
2024-06-11 00:28:09 +0600

    :zap: chore: Add StockData Model

--- api/core/models.py
+++ api/core/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
-# Create your models here.
+
+class StockData(models.Model):
+    date = models.DateField()
+    trade_code = models.CharField(max_length=20)
+    high = models.DecimalField(max_digits=10, decimal_places=2)
+    low = models.DecimalField(max_digits=10, decimal_places=2)
+    open = models.DecimalField(max_digits=10, decimal_places=2)
+    close = models.DecimalField(max_digits=10, decimal_places=2)
+    volume = models.PositiveIntegerField()
+
+    def __str__(self):
+        return f"{self.trade_code} on {self.date}"
