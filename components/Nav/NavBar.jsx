59e548ac49e68a0675ee60f87789dbf73c0dd790
Rakibul Islam <jis4nx@gmail.com>
2024-06-11 21:26:48 +0600

    :zap: chore: Nav Link For HomePage

--- components/Nav/NavBar.jsx
+++ components/Nav/NavBar.jsx
@@ -11,7 +11,7 @@ import {
 
 export default function NavBar() {
   const [openNav, setOpenNav] = React.useState(false);
-  const [mount, setMount] = React.useState()
+  const [mount, setMount] = React.useState();
 
   React.useEffect(() => {
     window.addEventListener(
@@ -39,13 +39,11 @@ export default function NavBar() {
   return mount ? (
     <Navbar className="mx-auto w-full px-4 py-2 lg:px-8 lg:py-4 mt-2">
       <div className="container mx-auto flex items-center justify-between text-blue-gray-900">
-        <Typography
-          as="a"
-          href="#"
-          className="mr-4 cursor-pointer py-1.5 font-medium"
-        >
-          StockMarket Data
-        </Typography>
+        <Link href={`/`}>
+          <Typography className="mr-4 cursor-pointer py-1.5 font-medium">
+            StockMarket Data
+          </Typography>
+        </Link>
         <div className="hidden lg:block">{navList}</div>
         <div className="flex items-center gap-x-1">
           <Button variant="text" size="sm" className="hidden lg:inline-block">
