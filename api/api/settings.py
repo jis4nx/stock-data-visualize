a7d6bf44e17be56b84cd440ab4fe96f2c2ea194c
Rakibul Islam <jis4nx@gmail.com>
2024-06-11 22:25:00 +0600

    :wrench: chore: Configure database connection

--- api/api/settings.py
+++ api/api/settings.py
@@ -10,6 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.0/ref/settings/
 """
 
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -78,13 +79,25 @@ WSGI_APPLICATION = "api.wsgi.application"
 # Database
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
-DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
-    }
-}
 
+def get_database_config():
+    if os.environ.get("PROD"):
+        return {
+            "ENGINE": "django.db.backends.postgresql",
+            "HOST": os.environ.get("DB_HOST", "localhost"),
+            "USER": os.environ.get("DB_USER", "postgres"),
+            "PASSWORD": os.environ.get("DB_PASSWORD", "postgres"),
+            "NAME": os.environ.get("DB_NAME", "postgres"),
+            "PORT": os.environ.get("DB_PORT", 5432),
+        }
+    else:
+        return {
+            "ENGINE": "django.db.backends.sqlite3",
+            "NAME": BASE_DIR / "db.sqlite3",
+        }
+
+
+DATABASES = {"default": get_database_config()}
 
 # Password validation
 # https://docs.djangoproject.com/en/5.0/ref/settings/#auth-password-validators
