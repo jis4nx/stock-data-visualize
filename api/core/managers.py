0c9776111baaf24ea5fe5c54d716a94d041a3e0c
Rakibul Islam <jis4nx@gmail.com>
2024-06-12 20:44:56 +0600

    :sparkles: chore: Documented views and model managers

--- api/core/managers.py
+++ api/core/managers.py
@@ -1,8 +1,20 @@
 from django.db import models
 
-
 class UniqueDateQuerySet(models.QuerySet):
+    """
+    Custom QuerySet to filter objects by unique dates.
+
+    Methods:
+        get_stocks_unique_date: Filters queryset to retrieve objects with unique dates.
+    """
+
     def get_stocks_unique_date(self):
+        """
+        Filters the queryset to retrieve objects with unique dates.
+
+        Returns:
+            QuerySet: Filtered queryset containing objects with unique dates.
+        """
         latest_stocks = self.values("date").annotate(max_id=models.Max("id"))
 
         latest_ids = [entry["max_id"] for entry in latest_stocks]
@@ -10,5 +22,13 @@ class UniqueDateQuerySet(models.QuerySet):
 
 
 class StockDataManager(models.Manager):
+    """
+    Custom Manager for StockData model.
+
+    Methods:
+        get_queryset: Returns the queryset using UniqueDateQuerySet.
+    """
+
     def get_queryset(self):
         return UniqueDateQuerySet(self.model, using=self._db)
+
--- api/core/views.py
+++ api/core/views.py
@@ -13,12 +13,27 @@ from .pagination import StandardResultsSetPagination
 
 
 class Home(APIView):
+    """
+    Home API endpoint.
+
+    Returns:
+        Response: A JSON response with a greeting message.
+    """
+
     def get(self, request):
         return Response({"msg": "Hello"})
 
 
 class StockDataViewSet(ModelViewSet):
-    # Sorting By date in descending order
+    """
+    ViewSet for StockData model.
+
+    Attributes:
+        filterset_fields (dict): Dictionary specifying filters for the view.
+        queryset (QuerySet): QuerySet for retrieving StockData objects.
+        serializer_class (Serializer): Serializer class for StockData model.
+    """
+
     filterset_fields = {
         "trade_code": ["iexact"],
         "date": ["exact"],
@@ -28,9 +43,22 @@ class StockDataViewSet(ModelViewSet):
 
 
 class ListStockDataPage(ListAPIView):
+    """
+    ListAPIView to retrieve filtered and paginated StockData objects.
+
+    Attributes:
+        pagination_class (Pagination): Pagination class for the view.
+    """
+
     pagination_class = StandardResultsSetPagination
 
     def get_queryset(self):
+        """
+        Get the queryset for retrieving StockData objects.
+
+        Returns:
+            QuerySet: Filtered queryset based on query parameters.
+        """
         queryset = StockData.objects.all()
         queryset = self.filter_by_trade_code(queryset)
         queryset = self.filter_by_year(queryset)
@@ -39,12 +67,30 @@ class ListStockDataPage(ListAPIView):
         return queryset
 
     def filter_by_trade_code(self, queryset):
+        """
+        Filter queryset by trade code.
+
+        Args:
+            queryset (QuerySet): Initial queryset.
+
+        Returns:
+            QuerySet: Filtered queryset by trade code.
+        """
         trade_code = self.request.query_params.get("trade_code")
         if trade_code:
             queryset = queryset.filter(trade_code=trade_code)
         return queryset
 
     def filter_by_year(self, queryset):
+        """
+        Filter queryset by year.
+
+        Args:
+            queryset (QuerySet): Initial queryset.
+
+        Returns:
+            QuerySet: Filtered queryset by year.
+        """
         years = self.request.query_params.get("year")
         if years:
             try:
@@ -56,6 +102,15 @@ class ListStockDataPage(ListAPIView):
         return queryset
 
     def filter_by_month(self, queryset):
+        """
+        Filter queryset by month.
+
+        Args:
+            queryset (QuerySet): Initial queryset.
+
+        Returns:
+            QuerySet: Filtered queryset by month.
+        """
         months = self.request.query_params.get("month")
         if months:
             try:
@@ -67,6 +122,15 @@ class ListStockDataPage(ListAPIView):
         return queryset
 
     def filter_unique(self, queryset):
+        """
+        Filter queryset to get unique entries or aggregate by date.
+
+        Args:
+            queryset (QuerySet): Initial queryset.
+
+        Returns:
+            QuerySet: Filtered queryset.
+        """
         unique = self.request.query_params.get("unique")
         if unique and unique.lower() == "true":
             queryset = queryset.get_stocks_unique_date()
@@ -77,6 +141,12 @@ class ListStockDataPage(ListAPIView):
         return queryset
 
     def get_serializer_class(self):
+        """
+        Get serializer class based on query parameters.
+
+        Returns:
+            Serializer: Serializer class based on query parameters.
+        """
         if self.request.query_params.get("unique", "").lower() == "true":
             return StockDataSerializer
         else:
@@ -84,6 +154,13 @@ class ListStockDataPage(ListAPIView):
 
 
 class TradeCodeListView(APIView):
+    """
+    API endpoint to retrieve a list of distinct trade codes.
+
+    Returns:
+        Response: A JSON response with a list of distinct trade codes.
+    """
+
     def get(self, request, format=None):
         trade_codes = StockData.objects.values_list("trade_code", flat=True).distinct()
 
