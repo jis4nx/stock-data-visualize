81a9082c32c72deecbba0cadf4abb34f46643003
Rakibul Islam <jis4nx@gmail.com>
2024-06-13 17:04:39 +0600

    :sparkles: refactor: Used transaction for atomicty & documented

--- api/core/management/commands/load_stock_data.py
+++ api/core/management/commands/load_stock_data.py
@@ -1,5 +1,6 @@
 import pandas as pd
 from django.core.management import BaseCommand
+from django.db import transaction
 
 from ...models import StockData
 
@@ -8,38 +9,63 @@ class Command(BaseCommand):
     help = "Load stock data from CSV"
 
     def add_arguments(self, parser):
+        """
+        Define the arguments that the command can take.
+
+        :param parser: Argument parser instance.
+        """
         parser.add_argument(
             "file_path", type=str, help="The file path of the data file to be loaded"
         )
 
     def handle(self, *args, **kwargs):
+        """
+        Entry point for the command. Handles the overall process of loading
+        data from a CSV file.
 
-        # Loading Stock market csv data to database
+        :param args: Additional arguments.
+        :param kwargs: Keyword arguments, including file_path.
+        """
         file_path = kwargs.get("file_path")
         self.load_from_csv(file_path)
 
     def load_from_csv(self, file_path):
-        try:
-            df = pd.read_csv(file_path)
-            df["high"] = df["high"].replace(",", "", regex=True).astype(float)
-            df["low"] = df["low"].replace(",", "", regex=True).astype(float)
-            df["open"] = df["open"].replace(",", "", regex=True).astype(float)
-            df["close"] = df["close"].replace(",", "", regex=True).astype(float)
-            df["volume"] = df["volume"].replace(",", "", regex=True).astype(int)
+        """
+        Loads stock data from a CSV file into the database.
 
-            stock_data_objects = [
-                StockData(
-                    date=row["date"],
-                    trade_code=row["trade_code"],
-                    high=row["high"],
-                    low=row["low"],
-                    open=row["open"],
-                    close=row["close"],
-                    volume=row["volume"],
+        :param file_path: Path to the CSV file containing stock data.
+        """
+        try:
+            chunk_size = 10000  # Number of rows to process at a time.
+            for chunk in pd.read_csv(file_path, chunksize=chunk_size):
+                # Clean and convert data types
+                chunk["high"] = chunk["high"].replace(",", "", regex=True).astype(float)
+                chunk["low"] = chunk["low"].replace(",", "", regex=True).astype(float)
+                chunk["open"] = chunk["open"].replace(",", "", regex=True).astype(float)
+                chunk["close"] = (
+                    chunk["close"].replace(",", "", regex=True).astype(float)
                 )
-                for _, row in df.iterrows()
-            ]
-            StockData.objects.bulk_create(stock_data_objects)
+                chunk["volume"] = (
+                    chunk["volume"].replace(",", "", regex=True).astype(int)
+                )
+
+                # Create a list of StockData instances
+                stock_data_objects = [
+                    StockData(
+                        date=row["date"],
+                        trade_code=row["trade_code"],
+                        high=row["high"],
+                        low=row["low"],
+                        open=row["open"],
+                        close=row["close"],
+                        volume=row["volume"],
+                    )
+                    for _, row in chunk.iterrows()
+                ]
+
+                with transaction.atomic():
+                    StockData.objects.bulk_create(stock_data_objects, batch_size=1000)
+
             self.stdout.write(self.style.SUCCESS("Data loaded successfully from CSV"))
         except Exception as e:
             self.stdout.write(self.style.ERROR(f"Error loading data from CSV: {e}"))
